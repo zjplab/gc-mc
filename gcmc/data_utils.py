4ec0cdd7809a69e6db9dab20d4ffce88ab65f0b5
JP Zhang <zjplab@gmail.com>
2019-04-03 10:22:46 -0400

    cooment it

--- gcmc/data_utils.py
+++ gcmc/data_utils.py
@@ -76,7 +76,7 @@ def download_dataset(dataset, files, data_dir):
 
         with ZipFile(StringIO(request.read())) as zip_ref:
             zip_ref.extractall('data/')
-        rename("./data/"+dataset.replace('_', '-'), "./data/"+dataset)
+        #rename("./data/"+dataset.replace('_', '-'), "./data/"+dataset)
         source = [target_dir + '/' + s for s in os.listdir(target_dir)]
         destination = data_dir+'/'
         for f in source:
